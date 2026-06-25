b10293032bde697099aea2a969fd7a3ea1906dc8
jomicaestudo23 <joemyson.cardoso@aluno.senai.br>
2026-06-25 14:44:45 -0300

    criando evento para armazenar formulario

--- js/script_veiculo.js
+++ js/script_veiculo.js
@@ -1,1 +1,17 @@
-    
+
+    const listVeiculo = document.querySelector('#div-lista-veiculo')
+    const crud = document.querySelector('#form-props')
+
+    crud.addEventListener('submit',(evt) => {
+        evt.preventDefault();
+
+        const inform = new FormData(crud)
+            
+
+
+
+
+
+
+
+    });
